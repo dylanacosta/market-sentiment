7003e8993db9e2885f80933b272d063928794272
dylanacosta <dylacosta05@gmail.com>
2025-09-09 20:59:27 +0800

    Update reddit.py

--- data-pipeline/scrapers/reddit.py
+++ data-pipeline/scrapers/reddit.py
@@ -14,6 +14,7 @@ def get_reddit_posts(subreddit_name="stocks", limit=10):
     
     Returns:
         list of dict: Each dict contains post title and score
+        Title: Title of the Reddit post
         Score: Number of upvotes minus downvotes
     """
     reddit = praw.Reddit(
